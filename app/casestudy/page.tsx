14ddb5bd16f9e51876f330901dcf11cb68ca40c4
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-11-20 15:08:13 +0500

    completed

--- app/casestudy/page.tsx
+++ app/casestudy/page.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import CaseStudies from '../components/CaseStudies'
+import React from "react";
+import CaseStudies from "../components/CaseStudies";
 
 export default function CaseStudy() {
   return (
-    <div className='w-full p-5'>
-        <div className='container mx-auto'>
-            <CaseStudies />
-        </div>
+    <div className="w-full p-5">
+      <div className="container mx-auto">
+        <CaseStudies />
+      </div>
     </div>
-  )
+  );
 }
